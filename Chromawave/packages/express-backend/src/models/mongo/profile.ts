8156a87c1c5ff1997c86ae45f46ea7434f632886
Aimee <aimee4312@gmail.com>
2024-02-15 12:28:30 -0800

    part c

---
+++ Chromawave/packages/express-backend/src/models/mongo/profile.ts
@@ -0,0 +1,17 @@
+import { Schema, Model, Document, model } from "mongoose";
+import { Profile } from "../profile";
+
+const profileSchema = new Schema<Profile>(
+  {
+    userid: { type: String, required: true, trim: true },
+    name: { type: String, required: true, trim: true },
+    nickname: { type: String, trim: true },
+    city: { type: String, trim: true }
+    airports: [String],
+  },
+  { collection: "user_profiles" }
+);
+
+const ProfileModel = model<Profile>("Profile", profileSchema);
+
+export default ProfileModel;
