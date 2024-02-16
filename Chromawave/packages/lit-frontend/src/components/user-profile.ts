e28921f1c20be1c341609be044f621301273ed70
Aimee <aimee4312@gmail.com>
2024-02-15 19:16:43 -0800

    component shows currently

--- Chromawave/packages/lit-frontend/src/components/user-profile.ts
+++ Chromawave/packages/lit-frontend/src/components/user-profile.ts
@@ -1,6 +1,7 @@
 import { css, html, LitElement } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
-import { Profile } from "../../express-backend/src/models/profile";
+import { Profile } from "../../../express-backend/src/models/profile";
+import { APIRequest, JSONRequest } from "../rest";
 
 @customElement("user-profile")
 export class UserProfileElement extends LitElement {
@@ -29,12 +30,16 @@ export class UserProfileElement extends LitElement {
     }
 
   render() {
+    const {
+      name,
+      email,
+    } = (this.profile || {}) as Profile;
     return html`
     <div class="profile-container">
             <div class="profile-navbar">
                 <img src="../../source-images/randomuser.jpeg" class="profile-img">
                 <div class="name selected-prof-nav">
-                    <a href="./index.html">${this.profile?.name}</a>
+                    <a href="./index.html">${name}</a>
                 </div>
                 <div class="saved-songs name">
                     <a href="./saved-songs.html">Saved Songs</a>
@@ -44,12 +49,12 @@ export class UserProfileElement extends LitElement {
                 </div>
             </div>
             <img src="../../source-images/randomuser.jpeg" class="profile-img img-change">
-            <p class="display-name">${this.profile?.name}</p>
+            <p class="display-name">${name}</p>
             <div class="text-input">
                 Name
-                <input type="text" placeholder="${this.profile?.name}">
+                <input type="text" placeholder="${name}">
                 Email
-                <input type="email" placeholder="${this.profile?.email}">
+                <input type="email" placeholder="${email}">
                 New Password
                 <input type="password" placeholder="">
                 Confirm Password
@@ -59,7 +64,72 @@ export class UserProfileElement extends LitElement {
     `;
   }
 
-  static styles = css`...`;
+  static styles = css`
+  .display-name {
+    grid-column: 3;
+    grid-row: 1;
+    font-size: 3em;
+    align-self: end;
+    margin: 0;
+  }
+
+  .img-change {
+      grid-column: 2;
+  }
+
+  .name {
+      padding: 10%;
+      font-size: 2em;
+  }
+
+  .profile-container {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      grid-auto-rows: auto;
+  }
+
+  .profile-display {
+      grid-column: 2 / -1;
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+      grid-gap: 1rem;
+      margin: 5%;
+    }
+    
+  .profile-display a {
+  display: inline-block;
+  width: 100%;
+  }
+
+  .profile-img {
+      box-sizing: border-box;
+      border-radius: 150px;
+      border: 2px solid var(--primary-accent);
+      width: 40%;
+      margin-top: 10%;
+      margin-left: 25%;
+  }
+
+  .profile-navbar {
+      grid-column: 1;
+      grid-row-start: 1;
+      grid-row-end: 10;
+      background-color: var(--primary-background);
+  }
+
+  .selected-prof-nav {
+      grid-column: 1; 
+      grid-row: 2; 
+      background-color: var(--secondary-color);
+      border-top-right-radius: 20px;
+      border-bottom-right-radius: 20px;
+  }
+
+  .text-input {
+      grid-column: 3;
+      grid-row: 2;
+  }
+  `;
 
   _fetchData(path: string) {
     fetch(serverPath(path))
