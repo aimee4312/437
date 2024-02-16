294584f0eec2cefc12030fcd3ee551aff6df8e67
Aimee <aimee4312@gmail.com>
2024-02-15 20:52:41 -0800

    fixed the style

--- Chromawave/packages/lit-frontend/src/components/user-profile.ts
+++ Chromawave/packages/lit-frontend/src/components/user-profile.ts
@@ -65,6 +65,25 @@ export class UserProfileElement extends LitElement {
   }
 
   static styles = css`
+  body {
+    margin: 0;
+    background-color: var(--primary-background);
+    color: var(--secondary-text);
+  }
+
+  a:link,
+  a:visited {
+    color: var(--secondary-text);
+    text-decoration: none;
+  }
+
+  input {
+    margin: 10px;
+    margin-left: 0;
+    width: 100%;
+    height: 10%
+  }
+
   .display-name {
     grid-column: 3;
     grid-row: 1;
@@ -74,61 +93,62 @@ export class UserProfileElement extends LitElement {
   }
 
   .img-change {
-      grid-column: 2;
+    grid-column: 2;
   }
 
   .name {
-      padding: 10%;
-      font-size: 2em;
+    padding: 10%;
+    font-size: 2em;
   }
 
   .profile-container {
-      display: grid;
-      grid-template-columns: repeat(4, 1fr);
-      grid-auto-rows: auto;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: auto;
   }
 
   .profile-display {
-      grid-column: 2 / -1;
-      display: grid;
-      grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-      grid-gap: 1rem;
-      margin: 5%;
-    }
+    grid-column: 2 / -1;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    grid-gap: 1rem;
+    margin: 5%;
+  }
     
   .profile-display a {
-  display: inline-block;
-  width: 100%;
+    display: inline-block;
+    width: 100%;
   }
 
   .profile-img {
-      box-sizing: border-box;
-      border-radius: 150px;
-      border: 2px solid var(--primary-accent);
-      width: 40%;
-      margin-top: 10%;
-      margin-left: 25%;
+    box-sizing: border-box;
+    border-radius: 150px;
+    border: 2px solid var(--primary-accent);
+    width: 40%;
+    margin-top: 10%;
+    margin-left: 25%;
   }
 
   .profile-navbar {
-      grid-column: 1;
-      grid-row-start: 1;
-      grid-row-end: 10;
-      background-color: var(--primary-background);
+    grid-column: 1;
+    grid-row-start: 1;
+    grid-row-end: 10;
+    background-color: var(--primary-background);
   }
 
   .selected-prof-nav {
-      grid-column: 1; 
-      grid-row: 2; 
-      background-color: var(--secondary-color);
-      border-top-right-radius: 20px;
-      border-bottom-right-radius: 20px;
+    grid-column: 1; 
+    grid-row: 2; 
+    background-color: var(--secondary-color);
+    border-top-right-radius: 20px;
+    border-bottom-right-radius: 20px;
   }
 
   .text-input {
-      grid-column: 3;
-      grid-row: 2;
+    grid-column: 3;
+    grid-row: 2;
   }
+  
   `;
 
   _fetchData(path: string) {
@@ -142,5 +162,53 @@ export class UserProfileElement extends LitElement {
       .then((json: unknown) => {
           if (json) this.profile = json as Profile;
       });
+  }
+}
+
+@customElement("user-profile-edit")
+export class UserProfileEditElement extends UserProfileElement {
+  render() {
+    return html`<form @submit=${this._handleSubmit}>
+        <!-- fill in form here -->
+        <button type="submit">Submit</button>
+    </form> `;
+  }
+
+  static styles = css`...`
+  ;
+
+  _handleSubmit(ev: Event) {
+    ev.preventDefault(); // prevent browser from submitting form data itself
+
+    const target = ev.target as HTMLFormElement;
+    const formdata = new FormData(target);
+    const entries = Array.from(formdata.entries())
+      .map(([k, v]) => (v === "" ? [k] : [k, v]))
+      .map(([k, v]) =>
+        k === "airports"
+          ? [k, (v as string).split(",").map((s) => s.trim())]
+          : [k, v]
+      );
+    const json = Object.fromEntries(entries);
+
+    this._putData(json);
+  }
+
+  _putData(json: Profile) {
+    fetch(serverPath(this.path), {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(json)
+    })
+      .then((response) => {
+        if (response.status === 200) return response.json();
+        else return null;
+      })
+      .then((json: unknown) => {
+        if (json) this.profile = json as Profile;
+      })
+      .catch((err) =>
+        console.log("Failed to PUT form data", err)
+      );
   }
 }
