2990688929e484cc1303e5bb3f55d7472466aafe
Aimee <aimee4312@gmail.com>
2024-03-09 14:57:37 -0800

    smiskis collections page

--- SmiskiWorld/packages/express-backend/dist/routes/collections.js
+++ SmiskiWorld/packages/express-backend/dist/routes/collections.js
@@ -1,2 +1,36 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const collection_1 = __importDefault(require("../collection"));
+const router = express_1.default.Router();
+router.post("/", (req, res) => {
+    const newCollection = req.body;
+    collection_1.default
+        .create(newCollection)
+        .then((collection) => res.status(201).send(collection))
+        .catch((err) => res.status(500).send(err));
+});
+router.get("/collection/:collectionName", (req, res) => {
+    const { collectionName } = req.params;
+    collection_1.default
+        .get(collectionName)
+        .then((collections) => {
+        if (!collections)
+            throw "Not found";
+        else
+            res.json(collections);
+    })
+        .catch((err) => res.status(404).end());
+});
+router.put("/:collectionName", (req, res) => {
+    const { collectionName } = req.params;
+    const newCollection = req.body;
+    collection_1.default
+        .update(collectionName, newCollection)
+        .then((collections) => res.json(collections))
+        .catch((err) => res.status(404).end());
+});
+exports.default = router;
---
+++ SmiskiWorld/packages/express-backend/src/routes/collections.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response } from "express";
+import { Collections } from "ts-models";
+import collections from "../collection";
+
+const router = express.Router();
+
+router.post("/", (req: Request, res: Response) => {
+  const newCollection = req.body;
+
+  collections
+    .create(newCollection)
+    .then((collection: Collections) => res.status(201).send(collection))
+    .catch((err) => res.status(500).send(err));
+});
+
+router.get("/collection/:collectionName", (req: Request, res: Response) => {
+  const { collectionName } = req.params;
+
+  collections
+    .get(collectionName)
+    .then((collections: Collections | undefined) => {
+      if (!collections) throw "Not found";
+      else res.json(collections);
+    })
+    .catch((err) => res.status(404).end());
+});
+
+router.put("/:collectionName", (req: Request, res: Response) => {
+  const { collectionName } = req.params;
+  const newCollection = req.body;
+
+  collections
+    .update(collectionName, newCollection)
+    .then((collections: Collections) => res.json(collections))
+    .catch((err) => res.status(404).end());
+});
+
+export default router;
